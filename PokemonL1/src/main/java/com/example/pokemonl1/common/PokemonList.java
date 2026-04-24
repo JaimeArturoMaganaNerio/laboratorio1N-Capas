98ce04a53e1c3618e994e5e11f031ab48b854fbd
JaimeArturoMaganaNerio <neriojaime9@gmail.com>
2026-04-24 10:54:14 -0600

    Feat: Agregando la lista de pokemons

--- PokemonL1/src/main/java/com/example/pokemonl1/common/PokemonList.java
+++ PokemonL1/src/main/java/com/example/pokemonl1/common/PokemonList.java
@@ -8,5 +8,45 @@ import java.util.List;
 
 @Component
 public class PokemonList {
+
     private final List<Pokemon> pokemon;
+
+    public PokemonListList() {
+        this.pokemon = new ArrayList<>();
+
+        this.pokemon.add(Pokemon.builder()
+                .nombre("Fuecoco")
+                .tipo("Fuego")
+                .zona("playa")
+                .region("Paldea")
+                .build());
+
+        this.pokemon.add(Pokemon.builder()
+                .nombre("Sprigatito")
+                .tipo("planta")
+                .zona("praderas")
+                .region("Paldea")
+                .build());
+
+        this.pokemon.add(Pokemon.builder()
+                .nombre("quaxly")
+                .tipo("agua")
+                .zona("playa")
+                .region("Paldea")
+                .build());
+
+
+        this.pokemon.add(Pokemon.builder()
+                .nombre("Ceruledge")
+                .tipo("Fuego y fantasma")
+                .zona("Zona sur de paldea")
+                .region("Paldea")
+                .build());
+
+    }
+
+    public List<Pokemon> getPokemon() {
+        return pokemon;
+    }
+
 }
