b5d21e8ae9c1d37047e0975aeca82bbd156bee52
JaimeArturoMaganaNerio <neriojaime9@gmail.com>
2026-04-24 10:31:37 -0600

    Feat: Agregando la clase Pokemon

---
+++ PokemonL1/src/main/java/com/example/pokemonl1/PokemonL1Application.java
@@ -0,0 +1,13 @@
+package com.example.pokemonl1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonL1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokemonL1Application.class, args);
+    }
+
+}
---
+++ PokemonL1/src/test/java/com/example/pokemonl1/PokemonL1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pokemonl1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonL1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
